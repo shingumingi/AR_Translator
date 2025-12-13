def6221b1f8f42a0adbf39a91b0b635ea53c4165
shingumingi <mingi3is@g.shingu.ac.kr>
2025-12-14 02:41:15 +0900

    [Fix] 일본어 애매하게 되는거 확실하게 고침

--- Assets/Scripts/MainWaiting.cs
+++ Assets/Scripts/MainWaiting.cs
@@ -4,13 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class MainWaiting : MonoBehaviour
 {
-    // 버튼의 OnClick 이벤트에 연결할 함수
     public void ChangeSceneWithDelay()
     {
         StartCoroutine(LoadSceneAfterDelay());
     }
 
-    // 0.2초 대기 후 씬을 로드하는 코루틴
     IEnumerator LoadSceneAfterDelay()
     {
         // 0.2초 기다림
